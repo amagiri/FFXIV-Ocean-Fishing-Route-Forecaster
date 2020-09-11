c05bdd8c2245408c715006345c27144bbbaa1472
amagiri <amagiri@users.noreply.github.com>
2020-09-10 23:35:51 -0700

    Datetime Traversal and Route Display

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
 var datetimeLocale = require('datetime-locale');
-var refTime = new Date("2020-09-03T16:00:00Z");
+var routeList = new Array();
+var refRoute;
 window.onload = function () {
+    var routesURL = 'json/routes.json';
+    var routesRequest = new XMLHttpRequest();
+    routesRequest.overrideMimeType("application/json");
+    routesRequest.open('GET', routesURL);
+    routesRequest.send();
+    routesRequest.onload = function () {
+        var response = routesRequest.response;
+        var parsedRoutes = JSON.parse(response);
+        var routeKeys = Object.keys(parsedRoutes);
+        routeKeys.forEach(function (route) {
+            var name = parsedRoutes[route].routeName;
+            var time = parsedRoutes[route].routeTime;
+            var type = parsedRoutes[route].routeType;
+            var key = route;
+            var reference = new Route(name, time, type, key);
+            routeList.push(reference);
+        });
+        refRoute = getRouteByKey('sunsetMerlthor', routeList);
+        refRoute.datetime = new Date("2020-09-03T16:00:00Z");
+    };
     var fromDate = new Date();
     var displayFromDate = datetimeLocale.toString(fromDate);
     displayFromDate = displayFromDate.slice(0, displayFromDate.lastIndexOf(":"));
@@ -22,11 +43,166 @@ function formValidation() {
         alert('No routes selected.');
         return inputCheck;
     }
+    return inputCheck;
 }
+function getSpecifiedRoutes() {
+    var inputKeys = getRouteInputs();
+    var inputTimespan = getDateInputs();
+    adjustTimespan(refRoute, inputTimespan);
+    var validRoutes = findAllRoutes(inputTimespan);
+    validRoutes = filterRoutes(validRoutes, inputKeys);
+    displayRoutes(validRoutes);
+}
+function findAllRoutes(timespan) {
+    var outputRoutes = new Array();
+    var currentTime = timespan.start;
+    while (timespan.end.getTime() - currentTime.getTime() > 0) {
+        outputRoutes.push(getRoute(refRoute, currentTime));
+        currentTime.setHours(currentTime.getHours() + 2);
+    }
+    return outputRoutes;
+}
+function filterRoutes(routeList, inputKeys) {
+    var filteredRoutes = new Array();
+    routeList.forEach(function (route) {
+        if (inputKeys.includes(route.key)) {
+            filteredRoutes.push(route);
+        }
+    });
+    return filteredRoutes;
+}
+function getRouteInputs() {
+    var selectedRoutes = new Array();
+    $('input[type="checkbox"]').each(function () {
+        if ($(this).prop("checked"))
+            selectedRoutes.push($(this).attr('id'));
+    });
+    return selectedRoutes;
+}
+function getDateInputs() {
+    var startDatetime = new Date($('#dateFrom').val().toString());
+    var endDatetime = new Date($('#dateTo').val().toString());
+    return new Period(startDatetime, endDatetime);
+}
+function adjustTimespan(refRoute, timespan) {
+    timespan.start.setMinutes(0);
+    timespan.start.setHours(timespan.start.getHours() + 1);
+    timespan.end.setMinutes(0);
+    var refHour = refRoute.datetime.getHours();
+    if (((timespan.start.getHours() - refHour) % 2) != 0) {
+        timespan.start.setHours(timespan.start.getHours() + 1);
+    }
+    if (((timespan.end.getHours() - refHour) % 2) != 0) {
+        timespan.end.setHours(timespan.end.getHours() - 1);
+    }
+    return;
+}
+function getRoute(refRoute, inputTime) {
+    var timeElapsed = inputTime.getTime() - refRoute.datetime.getTime();
+    var hourConversion = 1000 * 60 * 60;
+    var totalHours = timeElapsed / hourConversion;
+    var daysElapsed = Math.floor(totalHours / 24);
+    var hoursPassed = totalHours % 24;
+    var dailySchedule = ['sunset', 'sunset', 'night', 'night', 'day', 'day'];
+    var sailingRoutes = ['Northern Strait of Merlthor', 'Open Rhotano Sea'];
+    var sailingTimes = ['sunset', 'night', 'day'];
+    var dailyStartTime = dailySchedule[daysElapsed % 6];
+    var dailyStartRoute = sailingRoutes[daysElapsed % 2];
+    var hourlyRoute;
+    if (hoursPassed % 4 > 0) {
+        var filteredRoute = sailingRoutes.slice(0);
+        filteredRoute.splice(filteredRoute.indexOf(dailyStartRoute), 1);
+        hourlyRoute = filteredRoute[0];
+    }
+    else {
+        hourlyRoute = dailyStartRoute;
+    }
+    var hourlyTime;
+    switch (hoursPassed % 12) {
+        case 0:
+        case 2:
+            hourlyTime = dailyStartTime;
+            break;
+        case 4:
+        case 6:
+            var skip = 1;
+            if ((sailingTimes.indexOf(dailyStartTime) + skip) < sailingTimes.length) {
+                var i = sailingTimes.indexOf(dailyStartTime) + skip;
+                hourlyTime = sailingTimes[i];
+            }
+            else {
+                var i = sailingTimes.indexOf(dailyStartTime) - (sailingTimes.length - skip);
+                hourlyTime = sailingTimes[i];
+            }
+            break;
+        case 8:
+        case 10:
+            var skip = 2;
+            if ((sailingTimes.indexOf(dailyStartTime) + skip) < sailingTimes.length) {
+                var i = sailingTimes.indexOf(dailyStartTime) + skip;
+                hourlyTime = sailingTimes[i];
+            }
+            else {
+                var i = sailingTimes.indexOf(dailyStartTime) - (sailingTimes.length - skip);
+                hourlyTime = sailingTimes[i];
+            }
+            break;
+        default:
+            console.log('LOL SHRUG');
+            break;
+    }
+    var currentRoute = getRouteByNameTime(hourlyRoute, hourlyTime);
+    currentRoute.datetime = new Date(inputTime);
+    return currentRoute;
+}
+function displayRoutes(routeList) {
+    var $results = $('#results tbody');
+    $results.text('');
+    routeList.forEach(function (route) {
+        var $routeCell = $('<td></td>').text(route.routeName);
+        var $timeCell = $('<td></td>').text(route.datetime.toString());
+        var $row = $('<tr></tr>').append($routeCell).append($timeCell);
+        $results.append($row);
+    });
+}
+function getRouteByKey(routeKey, routeList) {
+    for (var i = 0; i < routeList.length; i++) {
+        if (routeKey === routeList[i].key) {
+            var route = JSON.parse(JSON.stringify(routeList[i]));
+            return route;
+        }
+    }
+}
+function getRouteByNameTime(routeType, routeTime) {
+    for (var i = 0; i < routeList.length; i++) {
+        if (routeType === routeList[i].routeType && routeTime === routeList[i].routeTime) {
+            var route = JSON.parse(JSON.stringify(routeList[i]));
+            return route;
+        }
+    }
+    console.log('bad times here.');
+}
+var Route = (function () {
+    function Route(name, time, type, key) {
+        this.routeName = name;
+        this.routeTime = time;
+        this.routeType = type;
+        this.key = key;
+    }
+    return Route;
+}());
+var Period = (function () {
+    function Period(start, end) {
+        this.start = start;
+        this.end = end;
+    }
+    return Period;
+}());
 $('#routeForm').submit(function (event) {
     event.preventDefault();
     if (!formValidation()) {
         return;
     }
+    getSpecifiedRoutes();
 });
 //# sourceMappingURL=main.js.map
--- main.ts
+++ main.ts
@@ -1,19 +1,45 @@
 const datetimeLocale = require('datetime-locale');  // Dependency
 
-const refTime = new Date("2020-09-03T16:00:00Z"); // Baseline time that everything will calculate off of
-// Anchor point is currently September 3, 2020 at 9am PDT
+var routeList: Route[] = new Array();
+
+// Create a baseline Route that everything else will be calculated off of
+var refRoute: Route; 
 
 window.onload = function() {
+    // Import route JSON
+    var routesURL = 'json/routes.json';
+    var routesRequest = new XMLHttpRequest();
+    routesRequest.overrideMimeType("application/json");
+    routesRequest.open('GET', routesURL);
+    routesRequest.send();
+    routesRequest.onload = function() {
+        var response = routesRequest.response;
+        var parsedRoutes = JSON.parse(response);
+        var routeKeys = Object.keys(parsedRoutes);
+        routeKeys.forEach(function(route) {
+            const name: string = parsedRoutes[route].routeName;
+            const time: string = parsedRoutes[route].routeTime;
+            const type: string = parsedRoutes[route].routeType;
+            const key: string = route;
+
+            var reference = new Route(name, time, type, key);
+            routeList.push(reference);  // Generates global variable list of current routes
+        });
+
+        refRoute = getRouteByKey('sunsetMerlthor', routeList);   // Anchor route is Northern Strait of Merlthor at sunset
+        refRoute.datetime = new Date("2020-09-03T16:00:00Z"); // Anchor time is currently September 3, 2020 at 9am PDT
+    };
+
     // Generate default from and to dates
     const fromDate = new Date();
-    var displayFromDate = datetimeLocale.toString(fromDate);  
+    var displayFromDate: string = datetimeLocale.toString(fromDate);  
     displayFromDate = displayFromDate.slice(0, displayFromDate.lastIndexOf(":"));   // Remove seconds from string
     $('#dateFrom').val(displayFromDate);
 
     var toDate = fromDate;
     const addedDays = 7;
     toDate.setDate(toDate.getDate() + addedDays); 
-    var displayToDate = datetimeLocale.toString(toDate);  
+    var displayToDate: string = datetimeLocale.toString(toDate);  
     displayToDate = displayToDate.slice(0, displayToDate.lastIndexOf(":"));   // Remove seconds from string
     $('#dateTo').val(displayToDate);
 
@@ -21,7 +47,7 @@ window.onload = function() {
 
 function formValidation() {
     // If no checkboxes are selected, return an error
-    var inputCheck = false;
+    var inputCheck: boolean = false;
     $('input[type="checkbox"]').each(function() {
         if ($(this).prop("checked"))
             inputCheck = true;
@@ -33,27 +59,216 @@ function formValidation() {
     }
 
     // Browser seems to check if datetime-local is filled correctly, so I won't touch on that.
+    return inputCheck;
 }
 
-function findRoutes() {
+// Main function
+function getSpecifiedRoutes() {
+    const inputKeys: string[] = getRouteInputs();
+    var inputTimespan: Period = getDateInputs();
+    adjustTimespan(refRoute, inputTimespan);    // Adjust timespan relative to the reference so that it starts and ends on a route time
 
+    var validRoutes = findAllRoutes(inputTimespan);
+    validRoutes = filterRoutes(validRoutes, inputKeys);
+
+    displayRoutes(validRoutes);
 }
 
-function recordInputs() {
-    var selectedRoutes = new Array();
+// Finds all routes
+function findAllRoutes(timespan: Period) {
+    var outputRoutes: Route[] = new Array();
+    var currentTime: Date = timespan.start;
+
+    while (timespan.end.getTime() - currentTime.getTime() > 0) {
+        outputRoutes.push(getRoute(refRoute, currentTime));
+
+        currentTime.setHours(currentTime.getHours() + 2);   // Move forward two hours
+    } 
+
+    return outputRoutes;
+}
+
+function filterRoutes(routeList: Route[], inputKeys: string[]) {
+    var filteredRoutes: Route[] = new Array();
+
+    routeList.forEach((route) => {
+        if (inputKeys.includes(route.key)) {
+            filteredRoutes.push(route);
+        }
+    })
+
+    return filteredRoutes;
+}
+
+function getRouteInputs() {
+    var selectedRoutes: string[] = new Array();
     $('input[type="checkbox"]').each(function() {
         if ($(this).prop("checked"))
             selectedRoutes.push($(this).attr('id'));
     })
 
+    return selectedRoutes;
 }
 
-function outputRoutes() {
-    $('#results tbody').text('');    // Clear old results
+function getDateInputs() {
+    const startDatetime = new Date($('#dateFrom').val().toString());
+    const endDatetime = new Date($('#dateTo').val().toString());
+
+    return new Period(startDatetime, endDatetime);
 }
+
+function adjustTimespan(refRoute: Route, timespan: Period) {
+    // Round start period up to the nearset hour
+    timespan.start.setMinutes(0);   
+    timespan.start.setHours(timespan.start.getHours() + 1);  
+    // If num in setHours(num) exceeds 23, the day will increment, so I will not check for overflow
+    // Likewise if it is less than 0, the day will decrement
+
+    timespan.end.setMinutes(0); // Round end period down
+
+    // Adjust start and end times as necessary so that they are in increments of 2 from the reference
+    const refHour = refRoute.datetime.getHours();
+
+    if (((timespan.start.getHours() - refHour) % 2) != 0) {
+        timespan.start.setHours(timespan.start.getHours() + 1); // Increment one hour if the difference is odd
+    }
+
+    if (((timespan.end.getHours() - refHour) % 2) != 0) {
+        timespan.end.setHours(timespan.end.getHours() - 1); // Decrement one hour if the difference is odd
+    }
+
+    return;
+}
+
+function getRoute(refRoute: Route, inputTime: Date) {
+    const timeElapsed: number = inputTime.getTime() - refRoute.datetime.getTime();
+    const hourConversion = 1000 * 60 * 60;
+    const totalHours = timeElapsed/hourConversion;  // This should always be a whole number due to our earlier rounding
+
+    const daysElapsed = Math.floor(totalHours/24);
+    const hoursPassed = totalHours % 24;
+
+    const dailySchedule = ['sunset', 'sunset', 'night', 'night', 'day', 'day'];
+    const sailingRoutes = ['Northern Strait of Merlthor', 'Open Rhotano Sea'];
+    const sailingTimes = ['sunset', 'night', 'day'];
+
+    const dailyStartTime = dailySchedule[daysElapsed % 6];
+    const dailyStartRoute = sailingRoutes[daysElapsed % 2];
+    
+    var hourlyRoute: string;
+    if (hoursPassed % 4 > 0) {  // 2 for flip, 0 for matching route
+        var filteredRoute = sailingRoutes.slice(0);
+        filteredRoute.splice(filteredRoute.indexOf(dailyStartRoute), 1);
+        hourlyRoute = filteredRoute[0];
+    }
+    else {
+        hourlyRoute = dailyStartRoute;
+    }
+
+    var hourlyTime: string;
+    switch (hoursPassed % 12) {
+        case 0:
+        case 2:
+            hourlyTime = dailyStartTime;
+            break;
+        case 4:
+        case 6:
+            var skip = 1;
+            if ((sailingTimes.indexOf(dailyStartTime) + skip) < sailingTimes.length) {
+                let i: number = sailingTimes.indexOf(dailyStartTime) + skip;
+                hourlyTime = sailingTimes[i];
+            }
+            else {
+                let i: number = sailingTimes.indexOf(dailyStartTime) - (sailingTimes.length - skip);    // Looping
+                hourlyTime = sailingTimes[i];
+            }
+            break;
+        case 8:
+        case 10:
+            var skip = 2;
+            if ((sailingTimes.indexOf(dailyStartTime) + skip) < sailingTimes.length) {
+                let i: number = sailingTimes.indexOf(dailyStartTime) + skip;
+                hourlyTime = sailingTimes[i];
+            }
+            else {
+                let i: number = sailingTimes.indexOf(dailyStartTime) - (sailingTimes.length - skip);    // Looping
+                hourlyTime = sailingTimes[i];
+            }
+            break;
+        default:
+            console.log('LOL SHRUG');
+            break;
+    }
+
+    var currentRoute = getRouteByNameTime(hourlyRoute, hourlyTime);
+    currentRoute.datetime = new Date(inputTime);
+
+    return currentRoute;
+}
+
+function displayRoutes(routeList: Route[]) {
+    const $results = $('#results tbody');
+    $results.text('');  // Clear out old results
+
+    routeList.forEach((route) => {
+        const $routeCell = $('<td></td>').text(route.routeName);
+        const $timeCell = $('<td></td>').text(route.datetime.toString());
+        const $row = $('<tr></tr>').append($routeCell).append($timeCell);
+        $results.append($row);
+    })
+}
+
+
+/* UTILITY FUNCTIONS */
+// Returns a route for a given key/id
+function getRouteByKey(routeKey: string, routeList: Route[]) {
+    for (let i = 0; i < routeList.length; i++) {
+        if (routeKey === routeList[i].key) {
+            const route: Route = JSON.parse(JSON.stringify(routeList[i]));
+
+            return route;
+        }
+    }
+}
+
+function getRouteByNameTime(routeType: string, routeTime: string) {
+    for (let i = 0; i < routeList.length; i++) {
+        if (routeType === routeList[i].routeType && routeTime === routeList[i].routeTime) {
+            const route: Route = JSON.parse(JSON.stringify(routeList[i]));
+
+            return route;
+        }
+    }
+
+    console.log('bad times here.');
+}
+
 
 /* CLASS DECLARATIONS */
+class Route {
+    routeName: string;
+    routeTime: string;
+    routeType: string;
+    key: string;
+    datetime: Date;
 
+    constructor(name: string, time: string, type: string, key: string) {
+        this.routeName = name;
+        this.routeTime = time;
+        this.routeType = type;
+        this.key = key;
+    }
+}
+
+class Period {
+    start: Date;
+    end: Date;
+
+    constructor(start: Date, end: Date) {
+        this.start = start;
+        this.end = end;
+    }
+}
 
 
 /* EVENT LISTENERS */
@@ -65,7 +280,7 @@ $('#routeForm').submit(function(event) {
     }
 
     // Run main function
-
+    getSpecifiedRoutes();
 })
 
 
